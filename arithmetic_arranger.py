fcc1f271c58479e8359ca2092463a63bc20e9dbe
chamodya wimansha <chamodyawimansha@gmail.com>
2020-12-11 20:21:34 +0530

    need to add row by row, 4 rows

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,7 +1,10 @@
 def arithmetic_arranger(problems, answers=False):
 
     arithmetics = []
-
+    row1 = ""
+    row2 = ""
+    row3 = ""
+    row4 = ""
     # breaking down the problems list into sub lists
     for item in problems:
         arithmetics.append(item.split(" "))
@@ -10,11 +13,13 @@ def arithmetic_arranger(problems, answers=False):
     if len(arithmetics) > 5:
         return ("Error: Too many problems.")
 
+    # looping the arithmetics list
     for items in arithmetics:
         # making sure numbers has only 4 digits
         for item in items:
             if len(item) > 4:
                 return ("Error: Numbers cannot be more than four digits.")
+
         # convert string to numbers. if can't return an error
         try:
             items[0] = int(items[0])
@@ -22,7 +27,26 @@ def arithmetic_arranger(problems, answers=False):
         except ValueError:
             return ("Error: Numbers must only contain digits.")
 
-    return arithmetics
+        maxLength = len(str(getMax(items)))
+        minLength = len(str(getMin(items)))
+
+        # adding row one
+
+        # adding row two
+
+        # adding row three
+
+        # adding row four
+
+    return (row1 + "\n" + row2 + "\n" + row3 + "\n" + row4)
+
+
+def getMax(list):
+    return max([list[0], list[2]])
+
+
+def getMin(list):
+    return min([list[0], list[2]])
 
 
 print(arithmetic_arranger(
